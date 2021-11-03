b8df16d5c140ec6085d50ba02d223fcf063dd788
Marge <marge.consunji@gmail.com>
2021-11-03 15:11:08 +0800

    Create flex container for dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import GlobalStyle from './style/globalStyle';
+import styled from 'styled-components';
+import data from './data/data';
+import Container from './components/Container';
+import Profile from './components/Profile';
+
+const Main = styled.main`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function App() {
+  console.log(data);
   return (
     <>
       <GlobalStyle />
-      Hello World text
+      <Main>
+        <Container>
+          <Profile data={data} />
+        </Container>
+      </Main>
     </>
   );
 }
