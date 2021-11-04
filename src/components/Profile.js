eb080778ae735b75d7d3c278cf380d8ea8d437cf
Marge <marge.consunji@gmail.com>
2021-11-04 14:37:26 +0800

    Handle filtering timeframes and set styling for active timeframe view

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -12,18 +12,19 @@ const ProfileContainer = styled.div`
 `;
 
 const ProfileInfo = styled.div`
+  width: 100%;
   background: var(--blue);
   display: flex;
   flex-direction: row;
   align-items: center;
-  padding: 2rem 2rem; 
+  padding: 1.8rem 1.8rem;
   border-radius: var(--curved-border);
 
   img {
     border: 3px solid var(--white);
     height: 70px;
     border-radius: 50%;
-    margin-right: 20px;
+    margin-right: 1rem;
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
   }
   
@@ -34,17 +35,38 @@ const ProfileInfo = styled.div`
   @media screen and ${breakpoints.l} {
     flex-direction: column;
     flex: 2 1 0;
+    align-items: flex-start;
+    justify-content: flex-end;
+
+    img {
+      margin-bottom: 2rem;
+    }
+
+    p {
+      margin-bottom: 0.6rem;
+    }
+
+    h1 {
+      max-width: 140px;
+      font-size: 2.2em;
+      margin-bottom: 2rem;
+    }
   }
 `;
 
 const ProfileFilter = styled.div`
   padding: 2rem 2rem;
   display: inline-flex;
+  flex-direction: row;
   width: 100%;
   justify-content: space-between;
 
   p {
     color: var(--blue-desat);
+
+    &.active {
+      color: var(--white);
+    }
 
     &:hover {
       cursor: pointer;
@@ -54,10 +76,14 @@ const ProfileFilter = styled.div`
   @media screen and ${breakpoints.l} {
     flex-direction: column;
     flex: 1 1 0;
+    
+    p {
+      padding: 0.5rem 0;
+    }
   }
 `;
 
-const Profile = () => {
+const Profile = ({ handleTime, time }) => {
 
   return (
     <ProfileContainer>
@@ -70,9 +96,9 @@ const Profile = () => {
 
       </ProfileInfo>
       <ProfileFilter>
-        <p>Daily</p>
-        <p>Weekly</p>
-        <p>Monthly</p>
+        <p id="daily" onClick={handleTime} className={`${time === 'daily' ? 'active' : ''}`}>Daily</p>
+        <p id="weekly" onClick={handleTime} className={`${time === 'weekly' ? 'active' : ''}`}>Weekly</p>
+        <p id="monthly" onClick={handleTime} className={`${time === 'monthly' ? 'active' : ''}`}>Monthly</p>
       </ProfileFilter>
     </ProfileContainer>
   );
