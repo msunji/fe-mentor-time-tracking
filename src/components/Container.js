001585e6ad90225e820a5674cac14a38562a2252
Marge <marge.consunji@gmail.com>
2021-11-04 13:23:58 +0800

    Set grid layout for screens larger than 649px but smaller than 840px

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,8 +1,38 @@
 import styled from 'styled-components';
+import breakpoints from '../style/breakpoints';
 
 const Container = styled.div`
+  padding: 4rem 0;
   width: 90%;
-  max-width: 1000px;
+  max-width: 1100px;
+  margin-left: auto;
+  margin-right: auto;
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto;
+  grid-template-areas: "profile"
+                        "."
+                        "."
+                        "."
+                        "."
+                        "."
+                        ".";
+
+  @media screen and ${breakpoints.l} {
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    grid-template-areas: "profile . . "
+                          "profile . . "
+                          "profile . . ";
+  }
+
+  @media screen and ${breakpoints.xl} {
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    grid-template-areas: "profile . . ."
+                         "profile . . .";
+  }
 `;
 
 export default Container;
